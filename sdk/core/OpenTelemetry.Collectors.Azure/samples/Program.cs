c12bb693b5ac0afd440a05f364e7d95bc36aa6a0
Pavel Krymets <pavel@krymets.com>
2019-07-15 16:19:10 -0700

    Add opentelemetry adapter

---
+++ sdk/core/OpenTelemetry.Collectors.Azure/samples/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading;
+using Azure.ApplicationModel.Configuration;
+using OpenTelemetry.Exporter.Zipkin;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Trace.Sampler;
+using src;
+
+namespace samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tracer = Tracing.Tracer;
+
+            var azureDiagnosticListener = new AzureDiagnosticListener(Tracing.Tracer);
+            azureDiagnosticListener.Subscribe();
+
+            var exporter = new ZipkinTraceExporter(
+                new ZipkinTraceExporterOptions()
+                {
+                    Endpoint = new Uri("http://127.0.0.1:9411/api/v2/spans"),
+                    ServiceName = typeof(Program).Assembly.GetName().Name,
+                },
+                Tracing.ExportComponent);
+            exporter.Start();
+
+            var appspan = tracer
+                .SpanBuilder("AppRun")
+                .SetSampler(Samplers.AlwaysSample)
+                .StartSpan();
+
+            using (var appscope = tracer.WithSpan(appspan))
+            {
+
+                var client = new ConfigurationClient(
+                    "Endpoint=https://pakrym-azconfig-ui.azconfig.io;Id=0-l1-s0:h5pHKElA5IjSL3z+FOYz;Secret=hZyskYCWgnmEYpkAMv/R67J2nl9y2wpxcEvwB4l0DHQ=");
+                client.Set("Sample_key", "Sample_value");
+
+                for (int i = 0; i < 10; i++)
+                {
+
+                    var span = tracer
+                        .SpanBuilder("incoming request " + i)
+                        .SetSampler(Samplers.AlwaysSample)
+                        .StartSpan();
+
+                    using (var scope = tracer.WithSpan(span))
+                    {
+                        Console.WriteLine(client.Get("Sample_key").Value.Value);
+
+                        Thread.Sleep(TimeSpan.FromSeconds(1));
+
+                        Console.WriteLine(client.Get("Sample_key").Value.Value);
+
+
+                        span.End();
+                    }
+                }
+            }
+
+            exporter.Stop();
+        }
+    }
+}
---
+++ sdk/core/OpenTelemetry.Collectors.Azure/src/AzureDiagnosticListener.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using OpenTelemetry.Context;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Trace.Sampler;
+
+namespace src
+{
+    public class AzureDiagnosticListener: IDisposable, IObserver<DiagnosticListener>, IObserver<KeyValuePair<string, object>>
+    {
+        private readonly ITracer _tracer;
+        // ugh
+        private readonly ConcurrentDictionary<Activity, IScope> _scopes = new ConcurrentDictionary<Activity, IScope>();
+
+        public AzureDiagnosticListener(ITracer tracer)
+        {
+            _tracer = tracer;
+        }
+
+        private IDisposable _subscription;
+
+        public void Subscribe()
+        {
+            if (_subscription == null)
+            {
+                _subscription = DiagnosticListener.AllListeners.Subscribe(this);
+            }
+        }
+
+        public void Dispose()
+        {
+            _subscription.Dispose();
+        }
+
+        public void OnCompleted()
+        {
+        }
+
+        public void OnError(Exception error)
+        {
+        }
+
+        public void OnNext(KeyValuePair<string, object> value)
+        {
+
+            if (value.Key.EndsWith("Start"))
+            {
+                OnStartActivity(Activity.Current, value.Value);
+            }
+            else if (value.Key.EndsWith("Stop"))
+            {
+                OnStopActivity(Activity.Current, value.Value);
+            }
+            else if (value.Key.EndsWith("Exception"))
+            {
+                OnException(Activity.Current, value.Value);
+            }
+            else
+            {
+                OnCustom(value.Key, Activity.Current, value.Value);
+            }
+        }
+
+        private void OnStartActivity(Activity current, object valueValue)
+        {
+            var span = _tracer.SpanBuilder(current.OperationName)
+                .SetSampler(Samplers.AlwaysSample)
+                .StartSpan();
+
+            _scopes.TryAdd(current, _tracer.WithSpan(span));
+        }
+
+        private void OnStopActivity(Activity current, object valueValue)
+        {
+            var span = _tracer.CurrentSpan;
+            foreach (var keyValuePair in current.Tags)
+            {
+                span.SetAttribute(keyValuePair.Key, keyValuePair.Value);
+            }
+
+            _scopes.TryRemove(current, out var scope);
+
+            scope?.Dispose();
+        }
+
+        private void OnException(Activity current, object valueValue)
+        {
+            var span = _tracer.CurrentSpan;
+            foreach (var keyValuePair in current.Tags)
+            {
+                span.SetAttribute(keyValuePair.Key, keyValuePair.Value);
+            }
+
+            span.Status= Status.Unknown;
+
+            _scopes.TryRemove(current, out var scope);
+            scope?.Dispose();
+        }
+
+        private void OnCustom(string valueKey, Activity current, object valueValue)
+        {
+        }
+
+        public void OnNext(DiagnosticListener value)
+        {
+            if (value.Name.StartsWith("Azure"))
+            {
+                value.Subscribe(this);
+            }
+        }
+    }
+}
