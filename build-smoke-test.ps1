c2bdd61dfe4fb33dbdd110dad6d6b1ed361fdb98
Pavel Krymets <pavel@krymets.com>
2019-10-15 16:02:25 -0700

    Even more automated

--- build-smoke-test.ps1
+++ build-smoke-test.ps1
@@ -1,2 +1,9 @@
 dotnet pack .\eng\services.proj
+$dir = "$pwd\artifacts\packages\Debug\"
+$updates = gci $dir -Filter *.nupkg | `
+    %{ [regex]::match($_, '([A-Za-z\.]+)\.(.*)\.nupkg') } | `
+    %{ "<PackageReference Update=`"$($_.Groups[1].Value)`" Version=`"$($_.Groups[2].Value)`" />`n" }
+
+$updates = "<Project>`n<ItemGroup>`n$updates</ItemGroup>`n</Project>"
+$updates | Out-File $pwd\PackageOverride.props
 dotnet publish .\samples\SmokeTest\ -restore -f netcoreapp2.1 -o smokeTestApp -p:OverridePackagesProps=$pwd\PackageOverride.props -p:AdditionalRestoreSources=$pwd\artifacts\packages\Debug\
